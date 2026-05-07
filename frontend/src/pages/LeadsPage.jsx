6c35bf8fd67b4e3bb966dd0539c71b1c1755239c
DNishadini <dinushikaherath150@gmail.com>
2026-05-08 00:09:34 +0530

    Initialize frontned

---
+++ frontend/src/pages/LeadsPage.jsx
@@ -0,0 +1,15 @@
+function LeadsPage() {
+
+    return (
+        <div className="p-10">
+
+            <h1 className="text-3xl font-bold">
+                Leads Page
+            </h1>
+
+        </div>
+    );
+
+}
+
+export default LeadsPage;
